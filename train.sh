da9f2405eeba1bf0e45020b28e6be6d57c59814a
Kieran Bamforth <hello@kieranbamforth.me>
2022-12-15 18:47:46 +0000

    Break newlines.

--- train.sh
+++ train.sh
@@ -13,9 +13,10 @@ cd $DREAMBOOTH && accelerate launch train_dreambooth.py \
   --instance_data_dir=$INSTANCE_DIR \
   --class_data_dir=$CLASS_DIR \
   --output_dir=$OUTPUT_DIR \
-  --with_prior_preservation --prior_loss_weight=1.0 \
   --instance_prompt="a photo of kieran" \
   --class_prompt="a photo of a man" \
+  --with_prior_preservation \
+  --prior_loss_weight=1.0 \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
