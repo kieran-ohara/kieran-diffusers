489a751df0393ca41bee992d0aba59a48256dab2
Kieran Bamforth <hello@kieranbamforth.me>
2022-12-16 18:16:28 +0000

    move prompts out of infer.py.

--- infer/infer.py
+++ infer/infer.py
@@ -2,25 +2,32 @@ from diffusers import StableDiffusionPipeline
 import torch
 import os
 
-model_name = "4002"
-model_id = os.getcwd()+"/model/"+model_name
 
-prompt = "kokieran man rides a bike, high definition, space, photo realistic, waterpaint"
-dir = model_name
-path = os.getcwd()+"/output/"+dir
+def setup_output_path(model_name):
+    output_path = os.getcwd()+"/infer/output/"+model_name
+    isExist = os.path.exists(output_path)
+    if not isExist:
+        os.makedirs(output_path)
+    return output_path
 
-pipe = StableDiffusionPipeline.from_pretrained(
-    model_id,
-    torch_dtype=torch.float16).to("cuda")
+def get_pipeline(model_name):
+    model_id = os.getcwd()+"/model/"+model_name
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16
+    )
+    cuda_pipe = pipe.to("cuda")
+    return cuda_pipe
 
+model_name = "1200"
 
-isExist = os.path.exists(path)
-if not isExist:
-   os.makedirs(path)
+output_path = setup_output_path(model_name)
+pipe = get_pipeline(model_name)
 
-for n in range(20):
-    image = pipe(
-        prompt,
-        num_inference_steps=50,
-        guidance_scale=7.5).images[0]
-    image.save(path+"/image"+str(n)+".png")
+with open('infer/prompts.txt') as prompts:
+    for prompt in prompts:
+        image = pipe(
+            prompt,
+            num_inference_steps=50,
+            guidance_scale=7.5).images[0]
+        image.save(output_path+"/image.png")
