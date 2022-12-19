617e6fbb5f89e78069fe85d69812ce0d8d74b97b
Kieran Bamforth <hello@kieranbamforth.me>
2022-12-19 18:15:45 +0000

    output results as a grid.

--- infer/infer.py
+++ infer/infer.py
@@ -1,42 +1,19 @@
 #!/bin/env python
 
-from diffusers import StableDiffusionPipeline
-from string import Template
-import torch
-import os
+import util
 
+MODEL_NAME = "1200"
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-PROJECT_ROOT = SCRIPT_DIR+"/.."
+pipe = util.get_pipeline(MODEL_NAME)
+with open(f'{util.script_dir()}/prompts.txt') as file:
+    prompts = [util.interpolate_prompt(line, instance='kokieran man') for line in file]
+    num_images_per_prompt = 1
 
-def output_path(model_name):
-    output_path = f'{SCRIPT_DIR}/output/{model_name}'
-    isExist = os.path.exists(output_path)
-    if not isExist:
-        os.makedirs(output_path)
-    return output_path
+    images = pipe(
+        prompts,
+        num_inference_steps=50,
+        num_images_per_prompt=num_images_per_prompt,
+        guidance_scale=7.5)
 
-def get_pipeline(model_name):
-    model_id = f'{PROJECT_ROOT}/model/{model_name}'
-    pipe = StableDiffusionPipeline.from_pretrained(
-        model_id,
-        torch_dtype=torch.float16
-    )
-    cuda_pipe = pipe.to("cuda")
-    return cuda_pipe
-
-
-model_name = "1200"
-pipe = get_pipeline(model_name)
-
-i = 0;
-with open(f'{SCRIPT_DIR}/prompts.txt') as prompts:
-    for prompt_template in prompts:
-        template = Template(prompt_template)
-        prompt = template.substitute(instance='kokieran man')
-        image = pipe(
-            prompt,
-            num_inference_steps=50,
-            guidance_scale=7.5).images[0]
-        image.save(f'{output_path(model_name)}/image{i}.png')
-        i = i+1
+    grid = util.image_grid(images.images, len(prompts), num_images_per_prompt)
+    grid.save(f'{util.output_path(MODEL_NAME)}.png')
