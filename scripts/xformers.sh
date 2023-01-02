9222f8920f98a70cdc73e820c1293980266a4653
Kieran Bamforth <hello@kieranbamforth.me>
2023-01-02 18:32:05 +0000

    Ensure is using sd environment for building xformers.

--- scripts/xformers.sh
+++ scripts/xformers.sh
@@ -10,6 +10,7 @@ fi
 
 eval "$(/"${HOME}"/anaconda3/bin/conda shell.bash hook)"
 source "/"${HOME}"/anaconda3/etc/profile.d/conda.sh"
+conda activate sd
 
 git clone https://github.com/facebookresearch/xformers.git ~/src/xformers
 cd ~/src/xformers
