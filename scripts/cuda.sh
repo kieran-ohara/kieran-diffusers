d7c121ff6dbf2da8dc9f955b49b515c7903b7d37
Kieran Bamforth <hello@kieranbamforth.me>
2023-01-03 17:23:20 +0000

    Add shebang to cuda install.

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # CUDA
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
