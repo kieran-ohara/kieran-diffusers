3eaf75e679a5a35a959205afc626f1f4c2b6ed94
Kieran Bamforth <hello@kieranbamforth.me>
2023-01-04 01:24:19 +0000

    Fix the build for centos.

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -14,3 +14,11 @@ cd ~/
 curl -L https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info > alacritty.info
 sudo tic -xe alacritty,alacritty-direct alacritty.info
 rm alacritty.info
+
+# To build CUDA
+sudo yum install -y centos-release-scl
+sudo yum install -y devtoolset-7-gcc*
+sudo yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r)
+
+# To view devices
+sudo yum install -y pciutils
--- scripts/xformers.sh
+++ scripts/xformers.sh
@@ -12,8 +12,10 @@ eval "$(/"${HOME}"/anaconda3/bin/conda shell.bash hook)"
 source "/"${HOME}"/anaconda3/etc/profile.d/conda.sh"
 conda activate sd
 
+
 git clone https://github.com/facebookresearch/xformers.git ~/src/xformers
 cd ~/src/xformers
 git submodule update --init --recursive
 pip install -r requirements.txt --no-cache-dir
+source /opt/rh/devtoolset-7/enable
 CUDA_HOME=/usr/local/cuda TORCH_CUDA_ARCH_LIST=$ARCH pip install -e .
