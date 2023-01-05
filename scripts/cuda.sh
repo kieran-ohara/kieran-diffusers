6845cb409ea4755d58222a4c33882abac92e9910
Kieran Bamforth <hello@kieranbamforth.me>
2023-01-05 19:40:52 +0000

    Move script to more apt place.

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# To build CUDA
+sudo yum install -y centos-release-scl
+sudo yum install -y devtoolset-7-gcc*
+sudo yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r)
+
+# yum-config-mgr
+sudo yum install -y yum-utils
+
 # CUDA
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,13 +6,5 @@ sudo yum install -y git
 # I want my physical computer to sleep while the training goes on.
 sudo yum install -y tmux
 
-# yum-config-mgr
-sudo yum install -y yum-utils
-
-# To build CUDA
-sudo yum install -y centos-release-scl
-sudo yum install -y devtoolset-7-gcc*
-sudo yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r)
-
 # To view devices
 sudo yum install -y pciutils
