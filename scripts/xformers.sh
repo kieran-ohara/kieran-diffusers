0db0364d77b371d154fff9806657c2c5691dda92
Kieran Bamforth <hello@kieranbamforth.me>
2023-01-05 18:53:44 +0000

    Remove part not required for build.

--- scripts/xformers.sh
+++ scripts/xformers.sh
@@ -1,13 +1,4 @@
 #!/bin/bash -xe
-
-# 8.6 for A10, 7.5 for T4. This can take some time.
-# see https://developer.nvidia.com/cuda-gpus
-if [ -z "$ARCH" ]
-then
-  echo "Please specify arch."
-      return 1
-fi
-
 eval "$(/"${HOME}"/anaconda3/bin/conda shell.bash hook)"
 source "/"${HOME}"/anaconda3/etc/profile.d/conda.sh"
 conda activate sd
