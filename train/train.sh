a22704fffbbc0c0ff6b8c2c3aaeaa7aba00a2ae3
kieran <kieran>
2023-01-07 19:33:43 +0000

    Update to use diffusers.

--- train/train.sh
+++ train/train.sh
@@ -4,8 +4,8 @@ THIS=$(pwd)
 DREAMBOOTH="${THIS}/submodules/diffusers/examples/dreambooth"
 
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export INSTANCE_DIR="${THIS}/data/instances/kieran2"
-export NUM_INSTANCES=2400
+export INSTANCE_DIR="${THIS}/instances/kieran3"
+export NUM_INSTANCES=228
 export CLASS_DIR="${THIS}/data/class/man${NUM_INSTANCES}"
 export OUTPUT_DIR="${THIS}/model"
 
@@ -21,11 +21,11 @@ cd $DREAMBOOTH && accelerate launch train_dreambooth.py \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
-  --learning_rate=5e-6 \
+  --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --num_class_images=$NUM_INSTANCES \
-  --max_train_steps=4400 \
+  --max_train_steps=1520 \
   --gradient_checkpointing \
   --mixed_precision=fp16 \
   --use_8bit_adam
